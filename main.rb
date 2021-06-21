432203c49e6167b4d7711d498679d80fbea91772
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-06-21 09:25:06 +0300

    Parse subcategories on category

--- main.rb
+++ main.rb
@@ -1,30 +1,44 @@
 require 'nokogiri'
 require 'open-uri'
+require 'byebug'
 
-urls = ['https://santehnika-online.ru/product/mebel_dlya_vannoy_stworki_montre_60_belaya/',
-        'https://spa-swim.ru/product/bellagio-luxury-cesano',
-        'https://www.santehnica.ru/product/195214.html',
-        'https://shower5.ru/collection/kabiny80x80']
+BASE_URL = 'https://santehnika-online.ru'
+category_urls = []
+urls = ['https://santehnika-online.ru/mebel_dlja_vannoj_komnaty/']
 
 def get_html(url)
   html = open(url) { |result| result.read }
   Nokogiri::HTML(html)
 end
 
-urls.each do |url|
-  page = get_html(url)
-  title = page.css('title').text
-  h1 = page.css('h1').text
-  nodeset  = page.css('head > meta').map do |node|
-   (node["name"] == "description") && node["content"]
+urls.each do |category|
+  category_urls << category
+  page = get_html(category)
+  subcategories = page.css('div.collapse-container.collapse-entered > div >' \
+                  'div > div > div > div > a').map do |a|
+                  BASE_URL + a['href']
   end
 
-  description = nodeset.select! { |item| item }
-  description = description ? description.join : ''
+  category_urls = category_urls + subcategories
 
-  puts "Title: #{title}"
-  puts "H1: #{h1}"
-  puts "Description: #{description}"
-  puts "URL: #{url}"
-  puts "=" * 40
+  p category_urls
+
+  category_urls.each do |url|
+    page = get_html(category)
+
+    title = page.css('title').text
+    h1 = page.css('h1').text
+    nodeset  = page.css('head > meta').map do |node|
+     (node["name"] == "description") && node["content"]
+    end
+
+    description = nodeset.select! { |item| item }
+    description = description ? description.join : ''
+
+    puts "Title: #{title}"
+    puts "H1: #{h1}"
+    puts "Description: #{description}"
+    puts "URL: #{url}"
+    puts "=" * 40
+  end
 end
